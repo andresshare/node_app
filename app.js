e9e7149d0c8f363401c04f152efca44ac1ac1c1f
andresshare <andresshare@gmail.com>
2018-01-19 21:45:26 -0500

    routes

--- app.js
+++ app.js
@@ -2,45 +2,59 @@ var express = require("express")
 var app = express()
 var User = require("./models/user").User
 var bodyParser = require("body-parser")
+var session= require("express-session")
+var router_app = require("./routes_app")
+var session_middleware = require("./middlewares/session")
 
-app.use(express.static('public'));
+
+app.use("/public",express.static('public'));
 
 
 app.use(bodyParser.json())//peticiones application/json
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+    secret: "123dfgdsfgs234234sdfs",
+    resave: false,
+    saveUninitialized: false
+
+}))
+
+// 
 
 app.set("view engine", "pug")
 
 app.get("/",function (req,res) {
+    console.log(req.session.user_id);
+
     res.render("index")
 })
 
-app.get("/login",function (req,res) {
+app.get("/signup",function (req,res) {
     User.find(function (err,doc) {
         console.log(doc);
+        res.render("signup")
+    })
+
+})
+
+app.get("/login", function (req, res) {
+    User.find(function (err, doc) {
         res.render("login")
     })
 
 })
 
 
-app.post("/users",function (req,res) {
-    var user = new User({
-        email: req.body.email,
-        password: req.body.password,
-        password_confirmation: req.body.password_confirmation,
-        username: req.body.username
+
+app.post("/sessions",function (req,res) {
+
+    User.findOne({email:req.body.email ,password:req.body.password},"username email",function (err,user) {
+        console.log(docs);
+        req.session.user_id = user._id
+        res.send("text")
     })
 
-    console.log(user.password_confirmation);
-
-    user.save(function(err,user,numero) {
-    if (err) {
-        console.log(String(err))
-    }
-        res.send("Datos recibidos")
-})
-
-
+    app.use("/app",session_middlewares)
+    app.use("/app", routes_app)
 })
 app.listen(3000)
